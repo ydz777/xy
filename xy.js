1bd642718692214fd6c83f7ed10ab14543d95456
ydz <67778879+yangdaozhao@users.noreply.github.com>
2022-10-16 01:04:24 +0800

    Update xy.js

--- xy.js
+++ xy.js
@@ -1,10 +1,10 @@
 var body = $response.body
 var url = $request.url
 
-如果（身体）{
-var obj = JSON.parse($response.body)
-obj.data = []
-$done({ body: JSON.stringify(obj) })
+if (body) {
+  var obj = JSON.parse($response.body)
+  obj.data = []
+  $done({ body: JSON.stringify(obj) })
 } else {
-$done（{}）
+  $done({})
 }
