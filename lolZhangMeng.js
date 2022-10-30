e46825dc333d93df5cf11e141a31cd78fbf77212
ydz <774404302@qq.com>
2022-10-30 18:15:47 +0800

    update

--- lolZhangMeng.js
+++ lolZhangMeng.js
@@ -5,6 +5,9 @@ if (body) {
   var obj = JSON.parse($response.body)
   obj.data.lolCard.tag1Content = 699111
   obj.data.lolCard.sumMatchNum = 699111
+  obj.data.lolCard.fullRankTitle = '最强王者'
+  obj.data.lolCard.rankUrl = 'https://ossweb-img.qq.com/images/lol/space/rank/2019pre/season_2019_challenger.png'
+
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
