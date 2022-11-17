91376a49d85c8795c1a8381a9b82c293c1d5593a
ydz <67778879+yangdaozhao@users.noreply.github.com>
2022-11-17 02:01:23 +0000

    update

--- yeguoyuedu.js
+++ yeguoyuedu.js
@@ -10,18 +10,4 @@ if (body) {
 }
 
 
-// {
-//   "msg" : "",
-//   "data" : {
-//     "left_ticket" : 1,
-//     "total_get_ticket" : "1",
-//     "summary" : [
-//       "书票是您在野果搜索的通行证。",
-//       "书票的获得:可以通过阅读更多章节，升级自身等级，完成每日任务获得。",
-//       "书票的用处:可以打赏给某本好书，激励作者。也可以花费书票成为VIP，享受更多超值服务。"
-//     ],
-//     "total_use_ticket" : 0
-//   },
-//   "code" : 200
-// }
-// https://mlol\.qt\.qq\.com/go/mine/card/get_list url response-body "sumMatchNum": "(.*)", response-body "sumMatchNum": "99999",
+ 
